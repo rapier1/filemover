3a47546d7ef439da997635aeb7734a658f05da81
Christopher J. Rapier <rapier@br005.pvt.bridges.psc.edu>
2020-09-09 09:18:02 -0400

    trying to get parsync to report on fpart wait time

--- filemover.pl
+++ filemover.pl
@@ -212,7 +212,7 @@ sub transport_command {
 	}
 	my $username = get_username();
 	$command .= "#copy parsyncfp log file for performance evaluation\n";
-	$command .= "cp ~/filemover_\${SLURM_JOB_ID}.log $config->{paths}->{cache}/$username.psync.cache/\n\n";
+	$command .= "cp \${SLURM_SUBMIT_DIR}/filemover_\${SLURM_JOB_ID}.log $config->{paths}->{cache}/$username.psync.cache/\n\n";
 	$command .= "#parse the user log file for performance data\n";
 	$command .= "$config->{paths}->{parsync_bindir}/logrunner.pl $config->{paths}->{cache}/$username.psync.cache $username \${SLURM_JOB_ID}\n\n";
 	return $command;
@@ -386,7 +386,7 @@ sub build_slurm_batch {
 #SBATCH --ntasks-per-node=$config->{slurmopts}->{ntasks}
 #SBATCH --nodes=$config->{slurmopts}->{nodes}
 #SBATCH --time=$config->{slurmopts}->{time}
-#SBATCH --output=~/$config->{slurmopts}->{output}
+#SBATCH --output=$config->{slurmopts}->{output}
 
 echo "User              = $username"
 echo "Date              = \$(date)"
